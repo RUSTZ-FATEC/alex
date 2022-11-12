24c6008348f8796c0da37f76883ceffb337704f3
victorkashima <victorkashima04@gmail.com>
2022-11-12 16:30:43 -0300

    I

--- server/routers/keywordRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const keywordController = require('../controllers/keywordController');
-
-router.get('/getKey', keywordController.getKey);
-router.post('/postKey', keywordController.postKey);
-
-module.exports = router;
